a99704787106ad325ead026bea47fe07b096c83d
jackyu12 <70978812+jackyu12@users.noreply.github.com>
2020-10-07 18:42:49 -0700

    Add files via upload

---
+++ assignment/Kth largest.java
@@ -0,0 +1,22 @@
+package hello;
+import java.util.*;
+public class test {
+	 public int findKthLargest(int[] nums, int k) {
+		   PriorityQueue<Integer> pq=new PriorityQueue<>();
+	        for(int n:nums){
+	           
+	            pq.add(n);
+	            if(pq.size()>k){
+	                pq.poll();
+	            }
+	            
+	        }
+	        
+	        
+	        return pq.peek();
+	        
+	    }}
+
+
+
+
